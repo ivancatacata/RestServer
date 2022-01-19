36edb54741c2f9bfc40253807a63c1073ed0a0fd
Ivancitou <ivancatacata.grupomax@gmail.com>
2022-01-19 14:02:49 +0000

    primer commit

---
+++ controllers/user.js
@@ -0,0 +1,37 @@
+const { response, request } = require("express");
+
+const usuariosGet = (req = request, res = response) => {
+  const { q, nombre = "no name", apikey } = req.query;
+  res.json({
+    msg: "get API - controlador",
+    q,
+    nombre,
+    apikey,
+  });
+};
+const usuariosPut = (req, res) => {
+  const { id } = req.params;
+  res.json({
+    msg: "put API - controlador",
+    id,
+  });
+};
+const usuariosPost = (req, res = response) => {
+  const { nombre, edad } = req.body;
+  res.json({
+    msg: "post API - controlador",
+    nombre,
+    edad,
+  });
+};
+const usuariosDelete = (req, res) => {
+  res.json({
+    msg: "delete API - controlador",
+  });
+};
+module.exports = {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+};
